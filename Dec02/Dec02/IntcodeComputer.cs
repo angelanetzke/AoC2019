325051a14e7a6c8bf46f1ea09297a4a0ee4e0f72
angelanetzke <angela.netzke@gmail.com>
2022-11-04 12:21:12 -0700

    Add day 2, part 1

---
+++ Dec02/Dec02/IntcodeComputer.cs
@@ -0,0 +1,51 @@
+﻿namespace Dec02
+{
+	internal class IntcodeComputer
+	{
+		private long[] memory = Array.Empty<long>();
+		private int position;
+
+		public void Run()
+		{
+			position = 0;
+			long opcode;
+			do
+			{
+				opcode = memory[position];
+				if (opcode == 1)
+				{
+					long operand1 = memory[memory[position + 1]];
+					long operand2 = memory[memory[position + 2]];
+					memory[memory[position + 3]] = operand1 + operand2;
+					position += 4;
+				}
+				else if (opcode == 2)
+				{
+					long operand1 = memory[memory[position + 1]];
+					long operand2 = memory[memory[position + 2]];
+					memory[memory[position + 3]] = operand1 * operand2;
+					position += 4;
+				}
+			} while (opcode != 99L);
+		}
+
+		public void SetMemory(string commands)
+		{
+			var commandArray = commands.Split(',');
+			memory = new long[commandArray.Length];
+			for (int i = 0; i < commandArray.Length; i++)
+			{
+				memory[i] = long.Parse(commandArray[i]);
+			}
+		}
+
+		public void SetValue(int position, long value)
+		{
+			memory[position] = value;
+		}
+		public long GetValue(int position)
+		{
+			return memory[position];
+		}
+	}
+}
---
+++ Dec02/Dec02/Program.cs
@@ -0,0 +1,15 @@
+﻿using Dec02;
+
+var inputLine = File.ReadAllText("input.txt");
+Part1(inputLine);
+
+void Part1(string inputLine)
+{
+	var computer = new IntcodeComputer();
+	computer.SetMemory(inputLine);
+	computer.SetValue(1, 12L);
+	computer.SetValue(2, 2L);
+	computer.Run();
+	var position0 = computer.GetValue(0);
+	Console.WriteLine($"Part 1: {position0}");
+}
