dac643f02c6aca41ac629a8959c16cac69543eff
angelanetzke <angela.netzke@gmail.com>
2022-11-04 13:52:16 -0700

    Add day 3, part 2

--- Dec03/Dec03/Program.cs
+++ Dec03/Dec03/Program.cs
@@ -1,9 +1,10 @@
 ﻿var allLines = File.ReadAllLines("input.txt");
-var line1 = allLines[0];
+var line1 =  allLines[0];
 var line2 = allLines[1];
-Part1(line1, line2);
+var intersections = Part1(line1, line2);
+Part2(line1, line2, intersections);
 
-static void Part1(string line1, string line2)
+static List<(int, int)> Part1(string line1, string line2)
 {
 	//(x, y), number of lines that cross that point
 	var points = new Dictionary<(int, int), int>();
@@ -97,14 +98,114 @@ static void Part1(string line1, string line2)
 				break;
 		}
 	}
+	var intersections = new List<(int, int)>();
 	int shortestDistance = int.MaxValue;
 	foreach ((int, int) thisPoint in points.Keys)
 	{
 		if (points[thisPoint] == 2)
 		{
+			intersections.Add(thisPoint);
 			int thisDistance = Math.Abs(thisPoint.Item1) + Math.Abs(thisPoint.Item2);
 			shortestDistance = Math.Min(shortestDistance, thisDistance);
 		}
 	}
 	Console.WriteLine($"Part 1: {shortestDistance}");
+	return intersections;
+}
+
+static void Part2(string line1, string line2, List<(int, int)> intersections)
+{
+	//(x, y), steps to this point
+	var line1Steps = new Dictionary<(int, int), int>();
+	var line2Steps = new Dictionary<(int, int), int>();
+	foreach((int, int) thisIntersection in intersections)
+	{
+		var x = 0;
+		var y = 0;
+		var steps = 0;
+		var line1Segments = line1.Split(',');
+		bool hasIntersectionBeenFound = false;
+		int segmentNumber = 0;
+		char thisDirection = 'U';
+		int stepsToGo = 0;		
+		while (!hasIntersectionBeenFound)
+		{
+			if (stepsToGo == 0)
+			{
+				thisDirection = line1Segments[segmentNumber].ToCharArray()[0];
+				stepsToGo = int.Parse(line1Segments[segmentNumber][1..]);
+				segmentNumber++;
+			}
+			steps++;
+			stepsToGo--;
+			switch (thisDirection)
+			{
+				case 'U':
+					y++;
+					break;
+				case 'D':
+					y--;
+					break;
+				case 'R':
+					x++;
+					break;
+				case 'L':
+					x--;
+					break;
+			}
+			if (thisIntersection.Equals((x, y)))
+			{
+				hasIntersectionBeenFound = true;
+			}
+		}
+		line1Steps[thisIntersection] = steps;
+	}
+	foreach ((int, int) thisIntersection in intersections)
+	{
+		var x = 0;
+		var y = 0;
+		var steps = 0;
+		var line2Segments = line2.Split(',');
+		bool hasIntersectionBeenFound = false;
+		int segmentNumber = 0;
+		char thisDirection = 'U';
+		int stepsToGo = 0;
+		while (!hasIntersectionBeenFound)
+		{
+			if (stepsToGo == 0)
+			{
+				thisDirection = line2Segments[segmentNumber].ToCharArray()[0];
+				stepsToGo = int.Parse(line2Segments[segmentNumber][1..]);
+				segmentNumber++;
+			}
+			steps++;
+			stepsToGo--;
+			switch (thisDirection)
+			{
+				case 'U':
+					y++;
+					break;
+				case 'D':
+					y--;
+					break;
+				case 'R':
+					x++;
+					break;
+				case 'L':
+					x--;
+					break;
+			}
+			if (thisIntersection.Equals((x, y)))
+			{
+				hasIntersectionBeenFound = true;
+			}
+		}
+		line2Steps[thisIntersection] = steps;
+	}
+	int fewestSteps = int.MaxValue;
+	foreach((int, int) thisIntersection in intersections)
+	{
+		fewestSteps = Math.Min(fewestSteps, line1Steps[thisIntersection] + line2Steps[thisIntersection]);
+	}
+	Console.WriteLine($"Part 2: {fewestSteps}");
 }
