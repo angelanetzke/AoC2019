b2483c079bfcd04e964a01b0d463d1eeb941371e
angelanetzke <angela.netzke@gmail.com>
2022-11-04 11:48:26 -0700

    Add day, part 2

--- Dec01/Dec01/Program.cs
+++ Dec01/Dec01/Program.cs
@@ -1,5 +1,6 @@
 ﻿var allLines = File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 void Part1(string[] allLines)
 {
@@ -11,4 +12,26 @@ void Part1(string[] allLines)
 		sum += thisFuel;
 	}
 	Console.WriteLine($"Part 1: {sum}");
+}
+
+void Part2(string[] allLines)
+{
+	long sum = 0L;
+	foreach (string thisLine in allLines)
+	{
+		sum += CalculateTotalFuel(long.Parse(thisLine));
+	}
+	Console.WriteLine($"Part 2: {sum}");
+}
+
+long CalculateTotalFuel(long mass)
+{
+	var sum = 0L;
+	var nextFuel = mass / 3 - 2;
+	while (nextFuel > 0)
+	{
+		sum += nextFuel;
+		nextFuel = nextFuel / 3 - 2;
+	}
+	return sum;
 }
