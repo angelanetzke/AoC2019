30a042a0e0147bc699753d9ba741417cd02df855
angelanetzke <angela.netzke@gmail.com>
2022-11-04 11:31:56 -0700

    Add day 1, part 1

---
+++ Dec01/Program.cs
@@ -0,0 +1,14 @@
+﻿var allLines = File.ReadAllLines("input.txt");
+Part1(allLines);
+
+void Part1(string[] allLines)
+{
+	long sum = 0L;
+	foreach (string thisLine in allLines)
+	{
+		var thisMass = long.Parse(thisLine);
+		var thisFuel = thisMass / 3 - 2;
+		sum += thisFuel;
+	}
+	Console.WriteLine($"Part 1: {sum}");
+}
