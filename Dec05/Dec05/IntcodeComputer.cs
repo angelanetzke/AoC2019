5123cc392a1d57db907c051995c8a7d6134b7aa0
angelanetzke <angela.netzke@gmail.com>
2022-11-04 20:09:35 -0700

    Add day 5, part 1

---
+++ Dec05/Dec05/IntcodeComputer.cs
@@ -0,0 +1,132 @@
+﻿namespace Dec05
+{
+	internal class IntcodeComputer
+	{
+		private long[] memory = Array.Empty<long>();
+		private int instructionPointer;
+		private long[] input = Array.Empty<long>();
+		private int inputPointer;
+		private List<long> output = new ();
+		public void Run()
+		{
+			instructionPointer = 0;
+			long opcode;
+			do
+			{
+				opcode = memory[instructionPointer] % 100;
+				switch (opcode)
+				{
+					case 1:
+						Add();
+						break;
+					case 2:
+						Multiply();
+						break;
+					case 3:
+						GetInput();
+						break;
+					case 4:
+						SetOutput();
+						break;
+				}
+			} while (opcode != 99L);
+		}
+
+		private void Add()
+		{
+			long parameter1;
+			long parameter2;
+			if (memory[instructionPointer] / 100 % 10 == 0)
+			{
+				parameter1 = memory[memory[instructionPointer + 1]];
+			}
+			else
+			{
+				parameter1 = memory[instructionPointer + 1];
+			}
+			if (memory[instructionPointer] / 1000 % 10 == 0)
+			{
+				parameter2 = memory[memory[instructionPointer + 2]];
+			}
+			else
+			{
+				parameter2 = memory[instructionPointer + 2];
+			}
+			memory[memory[instructionPointer + 3]] = parameter1 + parameter2;
+			instructionPointer += 4;
+		}
+
+		private void Multiply()
+		{
+			long parameter1;
+			long parameter2;
+			if (memory[instructionPointer] / 100 % 10 == 0)
+			{
+				parameter1 = memory[memory[instructionPointer + 1]];
+			}
+			else
+			{
+				parameter1 = memory[instructionPointer + 1];
+			}
+			if (memory[instructionPointer] / 1000 % 10 == 0)
+			{
+				parameter2 = memory[memory[instructionPointer + 2]];
+			}
+			else
+			{
+				parameter2 = memory[instructionPointer + 2];
+			}
+			memory[memory[instructionPointer + 3]] = parameter1 * parameter2;
+			instructionPointer += 4;
+		}
+
+		private void GetInput()
+		{
+			memory[memory[inputPointer + 1]] = input[inputPointer];
+			inputPointer++;
+			instructionPointer += 2;
+		}
+
+		private void SetOutput()
+		{
+			output.Add(memory[memory[instructionPointer + 1]]);
+			instructionPointer += 2;
+		}
+
+		public void SetMemory(string commands)
+		{
+			var commandArray = commands.Split(',');
+			memory = new long[commandArray.Length];
+			for (int i = 0; i < commandArray.Length; i++)
+			{
+				memory[i] = long.Parse(commandArray[i]);
+			}
+		}
+
+		public void SetInput(long[] input)
+		{
+			this.input = input;
+			inputPointer = 0;
+		}
+
+		public void ClearOutput()
+		{
+			output = new ();
+		}
+
+		public List<long> GetOutput()
+		{
+			return output;
+		}
+
+		public void SetValue(int position, long value)
+		{
+			memory[position] = value;
+		}
+		public long GetValue(int position)
+		{
+			return memory[position];
+		}
+	}
+
+}
---
+++ Dec05/Dec05/Program.cs
@@ -0,0 +1,14 @@
+﻿using Dec05;
+
+var inputLine = File.ReadAllText("input.txt");
+Part1(inputLine);
+
+static void Part1(string inputLine)
+{
+	var computer = new IntcodeComputer();
+	computer.SetMemory(inputLine);
+	computer.SetInput(new long[] { 1 });
+	computer.Run();
+	long diagnosticCode = computer.GetOutput().Last();
+	Console.WriteLine($"Part 1: {diagnosticCode}");	
+}
