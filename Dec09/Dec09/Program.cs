08e9210a5eb10b71b25be4434d010bba00a1123d
angelanetzke <angela.netzke@gmail.com>
2022-11-06 09:30:54 -0800

    Add day 9, part 2

--- Dec09/Dec09/Program.cs
+++ Dec09/Dec09/Program.cs
@@ -2,6 +2,7 @@
 
 var inputLine = File.ReadAllText("input.txt");
 Part1(inputLine);
+Part2(inputLine);
 
 static void Part1(string inputLine)
 {
@@ -11,6 +12,16 @@ static void Part1(string inputLine)
 	computer.Run();
 	var keycode = computer.GetOutput()[0];
 	Console.WriteLine($"Part 1: {keycode}");
+}
+
+static void Part2(string inputLine)
+{
+	var computer = new IntcodeComputer();
+	computer.SetMemory(inputLine);
+	computer.AddInput(2L);
+	computer.Run();
+	var keycode = computer.GetOutput()[0];
+	Console.WriteLine($"Part 2: {keycode}");
 }
 
 
