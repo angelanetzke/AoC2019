532ad20405e65a51bfe446e1d8e77c888eeec235
angelanetzke <angela.netzke@gmail.com>
2022-11-05 20:32:53 -0700

    Add day 8, part 1

---
+++ Dec08/Dec08/Image.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Dec08
+{
+	internal class Image
+	{
+		private readonly int WIDTH = 25;
+		private readonly int HEIGHT = 6;
+		private readonly string[] data;
+
+		public Image(string input)
+		{
+			int layerCount = input.Length / (WIDTH * HEIGHT);
+			data = new string[layerCount];
+			for (int i = 0; i < layerCount; i++)
+			{
+				data[i] = input.Substring(i * WIDTH * HEIGHT, WIDTH * HEIGHT);
+			}
+		}
+
+		public int GetLayerCount()
+		{
+			return data.Length;
+		}
+
+		public int CountDigits(int layer, int digit)
+		{
+			return Regex.Matches(data[layer], digit.ToString()).Count;
+		}
+
+	}
+}
---
+++ Dec08/Dec08/Program.cs
@@ -0,0 +1,23 @@
+﻿using Dec08;
+
+var inputLine = File.ReadAllText("input.txt");
+Part1(inputLine);
+
+static void Part1(string inputLine)
+{
+	var theImage = new Image(inputLine);
+	var layerWithLeastZeros = -1;
+	var leastZeros = int.MaxValue;
+	for (int i = 0; i < theImage.GetLayerCount(); i++)
+	{
+		int thisLayerZeros = theImage.CountDigits(i, 0);
+		if (thisLayerZeros < leastZeros)
+		{
+			layerWithLeastZeros = i;
+			leastZeros = thisLayerZeros;
+		}
+	}
+	var answer = theImage.CountDigits(layerWithLeastZeros, 1) * theImage.CountDigits(layerWithLeastZeros, 2);
+	Console.WriteLine($"Part 1: {answer}");
+}
+
