6c4b10c1a9ae0daf5015c71dc1b9aeea679a0ba1
angelanetzke <angela.netzke@gmail.com>
2022-11-06 16:40:42 -0800

    Add day 11, part 1

---
+++ Dec11/Dec11/Program.cs
@@ -0,0 +1,40 @@
+﻿using Dec11;
+
+var inputLine = File.ReadAllText("input.txt");
+Part1(inputLine);
+
+static void Part1(string inputLine)
+{
+	//(x, y), color (0 = black, 1 = white)
+	var theRobot = new Robot();
+	var tiles = new Dictionary<(int, int), int>();
+	var theComputer = new IntcodeComputer();
+	theComputer.SetMemory(inputLine);
+	theComputer.AddInput(0L);
+	int paintCount = 0;
+	IntcodeComputer.EXIT_STATUS currentStatus;
+	do
+	{
+		theComputer.ClearOutput();
+		currentStatus = theComputer.Run();
+		if (currentStatus == IntcodeComputer.EXIT_STATUS.PAUSE)
+		{
+			if (!tiles.ContainsKey(theRobot.GetPosition()))
+			{
+				paintCount++;
+			}
+			tiles[theRobot.GetPosition()] = (int)theComputer.GetOutput()[0];
+			theRobot.Rotate((int)theComputer.GetOutput()[1]);
+			theRobot.Move();
+			if (tiles.ContainsKey(theRobot.GetPosition()))
+			{
+				theComputer.AddInput(tiles[theRobot.GetPosition()]);
+			}
+			else
+			{
+				theComputer.AddInput(0);
+			}			
+		}
+	} while (currentStatus != IntcodeComputer.EXIT_STATUS.HALT);
+	Console.WriteLine($"Part 1: {paintCount}");
+}
---
+++ Dec11/Dec11/Robot.cs
@@ -0,0 +1,49 @@
+﻿namespace Dec11
+{
+	internal class Robot
+	{
+		private enum DIRECTION { N, E, S, W};
+		private DIRECTION currentDirection = DIRECTION.N;
+		private int x = 0;
+		private int y = 0;
+
+		public (int, int) GetPosition()
+		{
+			return (x, y);
+		}
+
+		public void Rotate(int instruction)
+		{
+			if (instruction == 0)
+			{
+				currentDirection = (DIRECTION)((int)(currentDirection + 3) % 4);
+			}
+			else
+			{
+				currentDirection = (DIRECTION)((int)(currentDirection + 1) % 4);
+			}
+		}
+
+		public void Move()
+		{
+			switch (currentDirection)
+			{
+				case DIRECTION.N:
+					y++;
+					break;
+				case DIRECTION.E:
+					x++;
+					break;
+				case DIRECTION.S:
+					y--;
+					break;
+				case DIRECTION.W:
+					x--;
+					break;
+			}
+		}
+
+	}
+
+
+}
