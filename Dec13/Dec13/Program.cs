1adc26d97c104b428e6b171c6c541061049283da
angelanetzke <angela.netzke@gmail.com>
2022-12-12 12:51:38 -0800

    Add day 13, part 1

---
+++ Dec13/Dec13/Program.cs
@@ -0,0 +1,21 @@
+﻿using Dec13;
+
+var allText = File.ReadAllText("input.txt");
+Part1(allText);
+
+static void Part1(string allText)
+{
+	var theComputer = new IntcodeComputer();
+	theComputer.SetMemory(allText);
+	theComputer.Run();
+	var theOutput = theComputer.GetOutput();
+	var blockCount = 0;
+	for (int i = 2; i < theOutput.Count; i += 3)
+	{
+		if (theOutput[i] == 2)
+		{
+			blockCount++;
+		}
+	}
+	Console.WriteLine($"Part 1: {blockCount}");
+}
